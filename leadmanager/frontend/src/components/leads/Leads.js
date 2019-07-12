03ad71d934b8b787d69d7737609cfe12e0dd5a03
hooheohee <45270575+hooheohee@users.noreply.github.com>
2019-07-11 22:19:08 -0400

    Update .gitignore

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -18,7 +18,7 @@ export class Leads extends Component {
     return (
       <Fragment>
         <h2>Leads</h2>
-        <table className="talbe table-striped">
+        <table className="table table-striped">
           <thead>
             <tr>
               <th>ID</th>
